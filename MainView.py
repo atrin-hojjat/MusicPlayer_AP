1f967c05241f8d6c333bbcc55d9e44f8a08b49da
Atrin Hojjat <atrin.hojjat@gmail.com>
2020-07-15 12:15:53 +0430

    Add player/playlist and timestamp functions

--- MainView.py
+++ MainView.py
@@ -12,14 +12,59 @@ class MainView(QMainWindow, Ui_MainWindow):
         super(MainView, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
+        self.timeLabelText, self.timeTot, self.timePos = "", "", ""
+
 
         # Player
+        self.player = QMediaPlayer()
+        self.player.error.connect(self.erroralert)
+
+        self.pauseButton.pressed.connect(self.player.pause)
+        self.playButton.pressed.connect(self.player.play)
+
+        self.timeSlider.valueChanged.connect(self.player.setPosition)
+        self.player.durationChanged.connect(self.updateDuration)
+        self.player.positionChanged.connect(self.updatePosition)
+
+
+        # PlayList
+        self.playlist = QMediaPlaylist()
+
+        self.player.setMediaPlaylist(self.playlist)
+
+        self.nextButton.pressed.connect(self.playlist.next)
+        self.previousButton.pressed.connect(self.playlist.previous)
+
+
 
         # Library List
 
         # Playlist list
 
         # Queue
+
+    def convertTime(self, time):
+        s = int(s / 1000)
+        m, s = divmod(s, 60)
+        h, m = divmod(m, 60)
+        return "{}:{}:{}".format(h, m, s)
+
+    def updateDuration(self, duration):
+        self.timeTot = self.convertTime(duration)
+        self.timeLabelText = "{} / {}".format(self.timePos, self.timeTot)
+
+        self.timeSlider.blockSignals(True)
+        self.timeSlider.setValue(timeLabelText)
+        self.timeSlider.blockSignals(False)
+
+    def updatePosition(self, position):
+        self.timePos = self.convertTime(position)
+        self.timeLabelText = "{} / {}".format(self.timePos, self.timeTot)
+
+        self.timeSlider.blockSignals(True)
+        self.timeSlider.setValue(timeLabelText)
+        self.timeSlider.blockSignals(False)
+
 
 
 if __name__ == "__main__":
