5b8b1ddf23f50243eb975ca231ea9d6d28af97e3
Atrin Hojjat <atrin.hojjat@gmail.com>
2020-07-15 21:35:46 +0430

    Add open file function, fix bugs with label, add playlist model

--- MainView.py
+++ MainView.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import *
 from PyQt5.Multimedia import *
 from PyQt5.MultimediaWidgets import *
 
+from PlayListModel import *
+
 from MainWindow import Ui_MainWindow
 
 
@@ -35,6 +37,14 @@ class MainView(QMainWindow, Ui_MainWindow):
         self.nextButton.pressed.connect(self.playlist.next)
         self.previousButton.pressed.connect(self.playlist.previous)
 
+        self.playlistModel = PlayListModel(self.playlist)
+        self.currentPlayList.setModel(self.playlistModel)
+        self.playlist.currentIndexChanged.connect(self.playlistModel.playListChange)
+
+        self.currentPlayList.selectionModel().selectionChenged.connect(self.playlistModel.selectionChanged)
+
+
+
 
 
         # Library List
@@ -43,6 +53,23 @@ class MainView(QMainWindow, Ui_MainWindow):
 
         # Queue
 
+    def open_files(self): 
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.AnyFile)
+        dialog.setFilter("Mp3 (*.mp3), WAV (*.wav)")
+
+        files = QStringList()
+
+        if dialog.exec_():
+            files = dialog.selectedFiles()
+
+        for f in files:
+            print(f)
+            self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(f)))
+
+        self.model.layoutChanged.emit()
+
+
     def convertTime(self, time):
         s = int(s / 1000)
         m, s = divmod(s, 60)
@@ -50,8 +77,11 @@ class MainView(QMainWindow, Ui_MainWindow):
         return "{}:{}:{}".format(h, m, s)
 
     def updateDuration(self, duration):
+        self.timeSlider.setMaximum(duration)
+
         self.timeTot = self.convertTime(duration)
         self.timeLabelText = "{} / {}".format(self.timePos, self.timeTot)
+        self.timeLabel.setText(self.timeLabelText)
 
         self.timeSlider.blockSignals(True)
         self.timeSlider.setValue(timeLabelText)
@@ -60,9 +90,10 @@ class MainView(QMainWindow, Ui_MainWindow):
     def updatePosition(self, position):
         self.timePos = self.convertTime(position)
         self.timeLabelText = "{} / {}".format(self.timePos, self.timeTot)
+        self.timeLabel.setText(self.timeLabelText)
 
         self.timeSlider.blockSignals(True)
-        self.timeSlider.setValue(timeLabelText)
+        self.timeSlider.setValue(position)
         self.timeSlider.blockSignals(False)
 
 
