18f2ffe9f3e7caada5e80b6c08fc4045dbe11699
Atrin Hojjat <atrin.hojjat@gmail.com>
2020-07-15 21:50:24 +0430

    Create basic playlist list model

---
+++ PlaylistListModel.py
@@ -0,0 +1,26 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.Multimedia import *
+from PyQt5.MultimediaWidgets import *
+
+class PlaylistListModel(QAbstractListModel):
+    def __init__(self, *arg, **kwargs):
+        super(PlayListModel, self).__init__(*args, *kwargs)
+        self.playlist = playlist
+
+    def data(self, ind, rl):
+        if rl == Qt.DisplayRole:
+
+
+            # Access Database Here
+
+
+            return dt.connectionUrl().filename()
+    
+    def rowCount(self, ind):
+
+        # Access Database Here
+
+        return 0
+
