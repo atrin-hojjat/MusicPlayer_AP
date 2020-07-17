9a6dd1d298460a929e6fafa4e160d9fc1ee9ac26
Atrin Hojjat <atrin.hojjat@gmail.com>
2020-07-17 09:23:14 +0430

    many bugs fixed

--- database.py
+++ database.py
@@ -11,6 +11,7 @@ def insert_song(song_name , artist , publish_year , album , address , gener) :
 
     first.execute("INSERT INTO music(song_name, artist, publish_year, album, address, gener) VALUES (? , ? , ? , ? , ? , ? ) ", 
         (song_name , artist , publish_year , album , address , gener )  ) 
+    connection.commit()
     
 
 #delete a song by it's id
@@ -22,6 +23,8 @@ def get_song_by_address(address):
 
 def delete_song_by_id(id ) :
     first.execute("DELETE from music WHERE rowid = ?" , (id, ) ) 
+    first.execute("DELETE from playlist_song WHERE track_id=?", (id,))
+    connection.commit()
 
 
 def get_songs():
@@ -93,7 +96,10 @@ def get_gener() :
 
 def get_playlist(id ) :
     
-    first.execute("SELETC * FROM playlist_song WHERE playlist_id = ? ", (id,) )
+    first.execute("""SELECT track_id, address FROM playlist_song inner join music on
+    playlist_song.track_id=music.id WHERE playlist_id = ? order by
+    playlist_song.track_number
+    """, (id,) )
     new_list = first.fetchall() 
     
 
@@ -103,7 +109,10 @@ def get_playlist(id ) :
 
 def get_playlists() :
 
-    first.execute("SELECT DISTINCT playlist_id FROM playlist_song " )
+    first.execute("SELECT DISTINCT playlist_song.playlist_id, playlist.playlist_name " + 
+            " FROM playlist_song " + 
+            "inner join playlist " +
+            "on playlist.id=playlist_song.playlist_id" )
     new_list = first.fetchall() 
 
 
@@ -114,14 +123,15 @@ def get_playlists() :
 def add_playlist(playlist_name , song_list ) :
 
     first.execute("INSERT INTO playlist(playlist_name) VALUES (?) " , (playlist_name,) ) 
-    first.execute("SELECT id , * FROM playlist LIMIT 1 ORDER BY DESC " ) 
     
-    element = first.fetchall()
+    element = first.lastrowid 
 
     new_list = []
     for i in range(len(song_list) ) :
-        new_list.append((i + 1 , song_list[i] , element[1], ))
-    first.execute("INSERT INTO Playlist_song(track_number, track_id, playlist_id) VALUES (? , ? , ? ) " , new_list) 
+        new_list.append((i + 1 , song_list[i] , element, ))
+    print(new_list)
+    first.executemany("INSERT INTO Playlist_song(track_number, track_id, playlist_id) VALUES (? , ? , ? ) " , new_list) 
+    connection.commit()
 
 
 #delete a playlist by its name which user gives us
@@ -129,7 +139,9 @@ def add_playlist(playlist_name , song_list ) :
 def delete_playlist(playlist_name ) :
 
     first.execute("DELETE FROM playlist WHERE id = ?" ,(playlist_name, ) )
-    first.execute("DELETE FROM playlist_song WHERE playlist_id = ? " (playlist_name ,) ) 
+    first.execute("DELETE FROM playlist_song WHERE playlist_id = ? ", (playlist_name ,) ) 
+
+    connection.commit()
 
 
 
